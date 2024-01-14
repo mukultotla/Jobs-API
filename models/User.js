b81d5c073d4d412a980208f2d92ff9c03e85ad7b
Mukul <mukultotla4@gmail.com>
2024-01-14 16:25:09 +0530

    user schema

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// Will do the schema validation using zod
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
