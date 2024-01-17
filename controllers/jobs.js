5adb3a6881bc60f80da906f1ba97d31310e9f109
Mukul <mukultotla4@gmail.com>
2024-01-17 22:32:30 +0530

    updated jobs controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,35 +1,46 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
 
 const getAllJobs = async (req, res) => {
-    res.json({
-      msg: "Get all jobs",
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+};
+const getJob = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({ createdBy: userId, _id: jobId });
+  if (!job) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      msg: "Job not found",
     });
-  };
-  const getJob = async (req, res) => {
-    res.json({
-      msg: "Get job",
-    });
-  };
-  const createJob = async (req, res) => {
-    res.json({
-      msg: "Create job",
-    });
-  };
-  const updateJob = async (req, res) => {
-    res.json({
-      msg: "Update job",
-    });
-  };
-  const deleteJob = async (req, res) => {
-    res.json({
-      msg: "Delete job",
-    });
-  };
-  
-  module.exports = {
-    getAllJobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob,
-  };
-  
+  }
+  res.status(StatusCodes.OK).json({
+    job,
+  });
+};
+const createJob = async (req, res) => {
+  // Input validation is written at model
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+const updateJob = async (req, res) => {
+  res.json({
+    msg: "Update job",
+  });
+};
+const deleteJob = async (req, res) => {
+  res.json({
+    msg: "Delete job",
+  });
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
