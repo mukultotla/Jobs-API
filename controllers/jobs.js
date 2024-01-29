7498d552bf3a1c19cfd4d4c3a3b9be514a7c9ad4
Mukul <mukultotla4@gmail.com>
2024-01-29 21:10:37 +0530

    update job and delete job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -6,19 +6,26 @@ const getAllJobs = async (req, res) => {
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
-  const {
-    user: { userId },
-    params: { id: jobId },
-  } = req;
-  const job = await Job.findOne({ createdBy: userId, _id: jobId });
-  if (!job) {
-    res.status(StatusCodes.NOT_FOUND).json({
-      msg: "Job not found",
+  try {
+    const {
+      user: { userId },
+      params: { id: jobId },
+    } = req;
+    const job = await Job.findOne({ createdBy: userId, _id: jobId });
+    if (!job) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        msg: "Job not found",
+      });
+    }
+    res.status(StatusCodes.OK).json({
+      job,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      msg: error.message,
     });
   }
-  res.status(StatusCodes.OK).json({
-    job,
-  });
 };
 const createJob = async (req, res) => {
   // Input validation is written at model
@@ -27,14 +34,44 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = async (req, res) => {
-  res.json({
-    msg: "Update job",
-  });
+  try {
+    const {
+      user: { userId },
+      params: { id: jobId },
+      body: { company, position },
+    } = req;
+    const job = await Job.findByIdAndUpdate(
+      { _id: jobId, createdBy: userId },
+      { company, position },
+      { new: true }
+    );
+    if (!job) {
+      throw new Error("Job not found");
+    }
+    res.status(StatusCodes.CREATED).json({
+      job,
+    });
+  } catch (error) {
+    res.json({
+      msg: error.message,
+    });
+  }
 };
 const deleteJob = async (req, res) => {
-  res.json({
-    msg: "Delete job",
-  });
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId });
+  if(!job) {
+    res.status(StatusCodes.NOT_FOUND).json({
+      msg: "Job not found"
+    })
+  }
+  res.status(StatusCodes.OK).json({
+    job, 
+    msg: "Job deleted"
+  })
 };
 
 module.exports = {
