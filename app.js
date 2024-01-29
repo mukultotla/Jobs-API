9b44a67e96f82e0130a5013bac7505d7dd7f836c
Mukul <mukultotla4@gmail.com>
2024-01-29 23:21:58 +0530

    adding security packages

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require('express-rate-limit');
+
 const authMiddleware = require("./middleware/authentication");
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -12,8 +16,14 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
+app.use(limiter);
 app.use(express.json());
-// extra packages
 
 // routes
 app.use("/api/v1/auth", authRouter);
@@ -21,7 +31,8 @@ app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-
+app.use(helmet());
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 const start = async () => {
